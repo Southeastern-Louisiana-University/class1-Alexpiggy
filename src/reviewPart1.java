d6e35b698b446773712578b84fa76d9f8ed75b5a
Alexpiggy <alexbittola@yahoo.com>
2025-01-23 12:16:41 -0600

    first commit

---
+++ src/reviewPart1.java
@@ -0,0 +1,16 @@
+package src;
+
+public class reviewPart1 {
+    public static void main (String args[]) {
+        double score = 2147483647.0;
+
+        System.out.println("Hello Java");
+        if (score >= 60.0) {
+            System.out.println("D");
+        }
+        else if (score >= 70) {
+            System.out.println("C");
+        }
+
+    }
+}
