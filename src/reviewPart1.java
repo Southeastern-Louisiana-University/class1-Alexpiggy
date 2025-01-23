750a835a1acb88fb3eeca5d01c50c4f1dac4315a
Alexpiggy <alexbittola@yahoo.com>
2025-01-23 12:26:58 -0600

    second commit

--- src/reviewPart1.java
+++ src/reviewPart1.java
@@ -1,15 +1,30 @@
 package src;
+import java.util.Scanner;
 
 public class reviewPart1 {
     public static void main (String args[]) {
-        double score = 2147483647.0;
+        System.out.println("Enter a grade from 0-100.");
 
-        System.out.println("Hello Java");
-        if (score >= 60.0) {
+        Scanner input = new Scanner(System.in);
+        double score = input.nextDouble();
+
+        if (score < 60.0) {
+            System.out.println("F");
+        }
+        else if (score < 70.0) {
             System.out.println("D");
         }
-        else if (score >= 70) {
+        else if (score < 80.0) {
             System.out.println("C");
+        }
+        else if (score < 90.0) {
+            System.out.println("B");
+        }
+        else if (score <= 100.0) {
+            System.out.println("A");
+        }
+        else {
+            System.out.println("Invalid input");
         }
 
     }
