f60fe8c487c45577a292417802c16765d7cce2a6
Alexpiggy <alexbittola@yahoo.com>
2025-02-06 12:17:49 -0600

    account commit 2
    (cherry picked from commit 346c3b89ed88b1bf660aad77cea4e87865600a41)

---
+++ src/Account.java
@@ -0,0 +1,25 @@
+package src;
+
+public class Account {
+    private String customer;
+    private double balance;
+
+    // Constructor
+    public Account(){};
+
+    public Account(String name, double balance) {
+        this.customer = name;
+        this.balance = balance;
+    }
+
+    public void PrintAccount() {
+        System.out.println("Account info"); // fix print
+    }
+
+    public static void main(String[] args) {
+        Account account = new Account();
+        Account account1 = new Account("Bou", 2033.89);
+        account.PrintAccount();
+        account1.PrintAccount();
+    }
+}
