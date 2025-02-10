91eb881667d8a11a1b9fb01afab36c0039ec4c14
Alexpiggy <alexbittola@yahoo.com>
2025-02-10 14:55:49 -0600

    class homework commit 1

--- src/Account.java
+++ src/Account.java
@@ -1,3 +1,7 @@
+// Homework:
+// provide appropriate get and set methods
+// modify printAccount as appropriate
+
 package src;
 
 public class Account {
@@ -13,12 +17,36 @@ public class Account {
     }
 
     public void PrintAccount() {
-        System.out.println("Account info"); // fix print
+        System.out.println("Account Info");
+        System.out.println("Customer: " + this.customer);
+        System.out.println("Balance: " + this.balance + "\n");
+    }
+
+    public void setCustomer(String name) {
+        this.customer = name;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomer() {
+        return this.customer;
+    }
+
+    public double getBalance() {
+        return this.balance;
     }
 
     public static void main(String[] args) {
         Account account = new Account();
+        account.setCustomer("Gregory");
+        account.setBalance(8.92);
+
         Account account1 = new Account("Bou", 2033.89);
+        System.out.println(account1.getCustomer());
+        System.out.println(account1.getBalance() + "\n");
+
         account.PrintAccount();
         account1.PrintAccount();
     }
