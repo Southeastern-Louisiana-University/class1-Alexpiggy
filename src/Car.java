b5c2df5cc5581374d10a6f4a3a6ca94219d85007
Alexpiggy <alexbittola@yahoo.com>
2025-02-13 14:43:41 -0600

    car homework commit 1

--- src/Car.java
+++ src/Car.java
@@ -19,11 +19,14 @@ public class Car {
     public static void main(String[] args) {
         Car car = new Car();
         Car carEthan = new Car("Hyundai", "VeraCruz", 2011);
+        Car carAlex = new Car("Ford", "CrownVictoria", 1998);
+        System.out.println("What is car.toString()? " + car.toString());
         System.out.println("What is carEthan.toString()? " + carEthan.toString());
+        System.out.println("What is carAlex.toString()? " + carAlex.toString());
 
     }
-    /*public String toString() {
-        return toString() + " new stuff\n";
-    }*/
+    public String toString() {
+        return "\nMake: " + this.make + "\nModel: " + this.model + "\nYear: " + this.year;
+    }
 
 }
